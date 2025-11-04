f1f13cde4632761ce86aed2192e11942efd93d39
AgarwalMayank2 <mayankagarwal020905@gmail.com>
2025-11-05 00:29:22 +0530

    basic_project

---
+++ My_project/main.py
@@ -0,0 +1,14 @@
+import numpy
+import pandas
+import matplotlib
+import utilis
+
+print("All the dependencies were imported successfully")
+
+number1 = float(input("Enter number 1:- "))
+number2 = float(input("Enter number 2:- "))
+
+print(f"Addition is {utilis.addition(number1, number2)}")
+print(f"Addition is {utilis.subtraction(number1, number2)}")
+print(f"Addition is {utilis.multiplication(number1, number2)}")
+print(f"Addition is {utilis.division(number1, number2)}")
---
+++ My_project/utilis.py
@@ -0,0 +1,11 @@
+def addition(n1: float, n2: float) -> float:
+    return n1 + n2
+
+def subtraction(n1: float, n2: float) -> float:
+    return n1 - n2
+
+def multiplication(n1: float, n2: float) -> float:
+    return n1*n2
+
+def division(n1: float, n2: float) -> float:
+    return n1/n2
